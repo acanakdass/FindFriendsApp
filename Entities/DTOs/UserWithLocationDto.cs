4d31726c7d5c26c544bedb2c408d433872d8d519
ahmetcanakdas <acanakdas@gmail.com>
2021-12-26 10:58:08 +0300

    dto,controller of location

---
+++ Entities/DTOs/UserWithLocationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Entities.Concrete;
+
+namespace Entities.DTOs
+{
+	public class UserWithLocationDto
+	{
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public bool Status { get; set; }
+        public Location Location { get; set; }
+    }
+}
+
---
+++ WebAPI/Controllers/LocationsController.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class LocationsController : Controller
+    {
+        private ILocationService _locationService;
+
+        public LocationsController(ILocationService locationService)
+        {
+            _locationService = locationService;
+        }
+
+
+        [HttpGet("getAll")]
+        public IActionResult GetAll()
+        {
+            var result = _locationService.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet("getById")]
+        public IActionResult GetById(int id)
+        {
+            var result = _locationService.GetById(id);
+            return Ok(result);
+        }
+
+
+        [HttpPost("add")]
+        public IActionResult Add([FromBody] Location location)
+        {
+            var result = _locationService.Add(location);
+            return Ok(result);
+        }
+
+        [HttpPost("createOrUpdate")]
+        public IActionResult CreateOrUpdate([FromBody] Location location)
+        {
+            var result = _locationService.CreateOrUpdate(location);
+            return Ok(result);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] Location location)
+        {
+            var result = _locationService.Update(location);
+            return Ok(result);
+        }
+
+    }
+}
+
